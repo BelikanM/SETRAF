d30ea6d018fcd948cb95d6c74dc9113de6d03fae
BelikanM <nyundumathryme@gmail.com>
2025-07-26 14:22:43 +0100

    Mise à jour des fichiers

---
+++ src/components/camera/IA/O.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IAcam from '../IAcam'; // import depuis camera/
+import '../camera.css';
+
+const O = () => {
+  return <IAcam />;
+};
+
+export default O;
---
+++ src/components/camera/IA/X.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Cam3D from '../Cam3D'; // import depuis camera/
+import '../camera.css';
+
+const X = () => {
+  return <Cam3D />;
+};
+
+export default X;
